31e0c63bbfde8816d19098fcc958483669bdc3b9
Nguyễn Khánh Linh (VMEC-KHT-CT VMEC) <Linhnk11@vingroup.net>
2019-04-12 21:03:11 +0700

    C:/Program Files/Git/

--- stadium/get_data.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-mongo_uri = "mongodb+srv://admin:admin@booking-z36ui.mongodb.net/test?retryWrites=true"
-
-client = MongoClient(mongo_uri)
-
-stadium_database = client.stadium_database
-stadium_collection = stadium_database["stadium_detail"]
-
