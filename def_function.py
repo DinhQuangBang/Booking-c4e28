20a7a093d063bdd13c7d309d9ff3a2b9d765985d
linhnk242 <khanhlinh24294@gmail.com>
2019-04-15 12:13:23 +0700

    .

--- def_function.py
+++ def_function.py
@@ -1,15 +1,3 @@
-
-
-customer_name = "Linh"
-customer_phone = "01665160194"
-customer_email = "khanhlinh24294@gmail.com"
-stadium_district = "Hai Ba Trung"
-stadium_name = "san bong"
-book_date = "11/04/2019"
-book_time = "6:00 - 7:30"
-stadium_email = "khanhlinh24294@gmail.com"
-
-
 def send_mail(customer_name,customer_phone,customer_email,stadium_district,stadium_name, book_date, book_time, stadium_email):
     import smtplib
     s = smtplib.SMTP('smtp.gmail.com', 587)
@@ -31,11 +19,3 @@ def send_mail(customer_name,customer_phone,customer_email,stadium_district,stadi
     s.sendmail(mail_address, customer_email, confirmation_message.encode("utf8"))
     s.sendmail(mail_address, stadium_email, notification_message.encode("utf8"))
     s.quit()
-
-send_mail(customer_name,customer_phone,customer_email,
-stadium_district,
-stadium_name,
-book_date,
-book_time,
-stadium_email,
-)
