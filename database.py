51e0c845f083ac426aed722ed52dbc9f0e48d0aa
nam11993 <hainam11993@gmail.com>
2019-04-16 20:56:12 +0700

    .

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from pymongo import MongoClient
 mongo_uri = "mongodb+srv://admin:admin@booking-z36ui.mongodb.net/test?retryWrites=true"
 client = MongoClient(mongo_uri)
@@ -7,3 +8,17 @@ stadium_database = client.stadium_database
 stadium_collection = stadium_database["stadium_detail"]
 user_database = client.user_database
 user_collection = user_database["user_information"]
+=======
+from pymongo import MongoClient
+mongo_uri = "mongodb+srv://admin:admin@booking-z36ui.mongodb.net/test?retryWrites=true"
+client = MongoClient(mongo_uri)
+form_database = client.submitted_form
+form_collection = form_database["form"]
+stadium_database = client.stadium_database
+stadium_collection = stadium_database["stadium_detail"]
+
+user_database = client.user_database
+user_collection = user_database["user_information"]
+
+
+>>>>>>> .
